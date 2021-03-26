5d574182ee2979d56aa34b140704b8a32d2ddf83
bond-spr <67597786+bond-spr@users.noreply.github.com>
2021-03-26 08:29:27 +0100

    homepage structure pc completely finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("script");
+window.onscroll = function (e) {  
+    if (document.documentElement.scrollTop > 5) {
+      document.getElementById("nav").style.backgroundColor = "#232323";
+      document.getElementById("nav").style.boxShadow = "0px 0px 25px #000000";
+    }
+    if (document.documentElement.scrollTop <= 5) {
+      document.getElementById("nav").style.backgroundColor = "transparent";
+      document.getElementById("nav").style.boxShadow = "0px 0px 25px transparent";
+    }
+  }
