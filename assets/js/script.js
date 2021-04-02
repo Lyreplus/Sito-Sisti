aef3843ae8ad89bc5325a65789f6c3001957dff2
bond-spr <67597786+bond-spr@users.noreply.github.com>
2021-04-02 17:56:01 +0200

    ultimated home grafics on mobile, solved some issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 window.onscroll = function (e) {  
     if (document.documentElement.scrollTop > 5) {
       document.getElementById("nav").style.backgroundColor = "#232323";
-      document.getElementById("nav").style.boxShadow = "0px 0px 25px #000000";
+      document.getElementById("nav").style.boxShadow = "0px 0px 35px #000000";
     }
     if (document.documentElement.scrollTop <= 5) {
       document.getElementById("nav").style.backgroundColor = "transparent";
-      document.getElementById("nav").style.boxShadow = "0px 0px 25px transparent";
+      document.getElementById("nav").style.boxShadow = "0px 0px 35px transparent";
     }
   }
 
