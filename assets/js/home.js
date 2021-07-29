5145a53e53c0eef8fc6087beac2a743fb4519018
Lorenzo Pattaro <lpzonta@gmail.com>
2021-07-30 01:18:22 +0200

    added chi siamo, dove siamo paragraphs and images

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,23 +1,23 @@
-window.onscroll = function (e) {
+window.onscroll = function(e) {
 
     if (document.documentElement.scrollTop > 5) {
-      document.getElementById("nav").style.backgroundColor = "#232323";
-      document.getElementById("nav").style.boxShadow = "0px 0px 35px #000000";
+        document.getElementById("nav").style.backgroundColor = "#232323";
+        document.getElementById("nav").style.boxShadow = "0px 0px 35px #000000";
     }
     if (document.documentElement.scrollTop <= 5) {
-      document.getElementById("nav").style.backgroundColor = "transparent";
-      document.getElementById("nav").style.boxShadow = "0px 0px 35px transparent";
+        document.getElementById("nav").style.backgroundColor = "transparent";
+        document.getElementById("nav").style.boxShadow = "0px 0px 35px transparent";
     }
-  }
+}
 
 const appearInfo = () => {
-    let i=0;
+    let i = 0;
     while (typeof(document.querySelectorAll('.img-info')[i]) != "undefined") {
         const parentDiv = document.querySelectorAll('.img-info')[i];
         const infoDiv = document.querySelectorAll('.placeholder')[i];
 
         parentDiv.addEventListener('click', () => {
-        infoDiv.classList.toggle('info');
+            infoDiv.classList.toggle('info');
         });
         i += 1;
     }
