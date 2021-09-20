30e70e9b04e48cabaeb185e69b411656251bf8ee
bond-spr <67597786+bond-spr@users.noreply.github.com>
2021-09-20 16:21:41 +0200

    corrections of mobile view and scrolling

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,4 @@
 window.onscroll = function (e) {  
-    let a = (document.documentElement.scrollTop)*0.5-500 + "px";
+    let a = (document.documentElement.scrollTop)*0.5-600 + "px";
     document.getElementById("parallax-img").style.marginTop = a;
 }
